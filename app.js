aa999183b1bca1ac588888f068c73845c98a713f
dusan.vesic <dusan.vesic@gmail.com>
2016-10-25 21:36:40 +0200

    'WIP'

--- app.js
+++ app.js
@@ -12,38 +12,50 @@ myAppModule.controller('TinyMceController', function ($scope) {
   };
 
   $scope.tinymceOptions = {
+    height: 500,
 
     setup: function (editor) {
       editor.on("init", function () {
         console.log(editor);
       });
 
-      editor.addButton('custom', {
+      editor.addButton('iframe', {
         type: 'button',
-        text: 'Webpage',
+        text: '<iframe />',
         icon: false,
         onclick: () => {
           editor.windowManager.open({
             title: 'Enter URL',
             body: [
-              { type: 'textbox', name: 'url', label: 'URL' }
+              { type: 'textbox', name: 'url', label: 'URL' },
+              { type: 'textbox', name: 'width', label: 'width' },
+              { type: 'textbox', name: 'height', label: 'height' },
             ],
-            onsubmit: function (e) {
-              // Insert content when the window form is submitted
-              // editor.insertContent('Title: ' + e.data.title);
-              console.log(e.data)
-              editor.insertContent(`
-                <iframe src="${e.data.url}"></iframe>
-              `)
+            onsubmit: e => {
+              let { url, width, height } = e.data;
+
+              // check input
+              if (url === '' || width === '' || height === '') {
+                alert('Check values!')
+                return;
+              } else {
+                // TODO: test NaN
+                width = parseInt(width, 10);
+                height = parseInt(height, 10);
+                editor.insertContent(`
+                  <iframe width="${width}"
+                          height="${height}"
+                          target='_top' src="${e.data.url}"></iframe>
+                `)
+              }
             }            
           })
         }
       })
-
-      
     },
 
     plugins: 'link image code',
-    toolbar: 'custom undo redo | bold italic | alignleft aligncenter alignright | code'
+    // menubar: 'file edit insert view format table tools',
+    toolbar: 'iframe | undo redo | bold italic | alignleft aligncenter alignright | code'
   };
 });
